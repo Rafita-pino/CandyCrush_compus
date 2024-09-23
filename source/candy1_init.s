cd13601734759f8a2bfb295c005fddd2717523a6
Rafa Pino Rodriguez <rafael.pinor@estudiants.urv.cat>
2024-09-23 17:04:04 +0200

    prog1: save

--- source/candy1_init.s
+++ source/candy1_init.s
@@ -1,8 +1,8 @@
 ﻿@;=                                                          	     	=
 @;=== candy1_init.s: rutinas para inicializar la matriz de juego	  ===
 @;=                                                           	    	=
-@;=== Programador tarea 1A: xxx.xxx@estudiants.urv.cat				  ===
-@;=== Programador tarea 1B: yyy.yyy@estudiants.urv.cat				  ===
+@;=== Programador tarea 1A: rafael.pinor@estudiants.urv.cat				  ===
+@;=== Programador tarea 1B: rafael.pinor@estudiants.urv.cat				  ===
 @;=                                                       	        	=
 
 
@@ -31,7 +31,7 @@
 @;TAREA 1A;
 @; inicializa_matriz(*matriz, num_mapa): rutina para inicializar la matriz de
 @;	juego, primero cargando el mapa de configuración indicado por parámetro (a
-@;	obtener de la variable global mapas[][][]) y después cargando las posiciones
+@;	obtener de la variable global mapas[][]) y después cargando las posiciones
 @;	libres (valor 0) o las posiciones de gelatina (valores 8 o 16) con valores
 @;	aleatorios entre 1 y 6 (+8 o +16, para gelatinas)
 @;	Restricciones:
@@ -39,16 +39,70 @@
 @;			mod_random()
 @;		* para evitar generar secuencias se invocará la rutina
 @;			cuenta_repeticiones() (ver fichero 'candy1_move.s')
-@;	Parámetros:
-@;		R0 = dirección base de la matriz de juego
-@;		R1 = número de mapa de configuración
+@;	Parmetros:
+@;		R0 = direccion base de la matriz de juego
+@;		R1 = numero de mapa de configuracion (para copiar)
+@;	Registros:
+@;		R0 = Valores para otras rutinas
+@;		R1 = Filas (i)
+@;		R2 = Columnas (j)
+@;		R3 = num mapa config // Bits 0..2 // Direccion cuenta repeticiones
+@;		R4 = Direccion base matriz juego
+@;		R5 = Direccion base del mapa config
+@;		R6 = Desplazamiento de posiciones
+@;		R7 = Valor de la casilla (R1,R2) 
+@;		R8 = valor per accedir a mapa config // Bits 4..3
 	.global inicializa_matriz
 inicializa_matriz:
-		push {lr}			@;guardar registros utilizados + link register
-		
-		
-		pop {pc}			@;recuperar registros y retornar al invocador
-
+		push {r0-r8, lr}		@;guardar registros utilizados
+		mov r4, r0				@; r0 = direccion base de la matriz
+		mov r3, r1				@; r3 = num mapa config				
+		ldr r5, =mapas
+		mov r8, #COLUMNS*ROWS	@; multiplicamos columnas por filas
+		mul r8, r3					@;multiplicamos el resultado anterior por el numero de mapa de configuracion
+		add r5, r8					@;accedemos al mapa
+		mov r6, #0		@; desplazamiento de posiciones
+		mov r1, #0		@; desplazamiento de filas (i)
+		.L_fori:
+			mov r2, #0	@; desplazamiento de columnas (j)
+			.L_forj:
+				ldrb r7, [r5, r6] @; r7= valor de la casilla (R1,R2) de mapa config
+				mov r3, r7
+				and r3, #0x07	  @; procesamos los bits 2..0 del contenido de r3 (R1,R2)
+				tst r3, #0x07
+				beq .L_random
+				strb r7, [r4, r6] @; guardamos el resultado en la matriz de juego si r3=! 000
+				b .L_fi			  @; pasamos hasta el final
+				
+			.L_random:
+				mov r3, #0
+				mov r0, #6			  @; pasamos rango maximo
+				bl mod_random		  
+				add r0, #1			  @; sumamos 1 para no obtener 0
+				add r3, r7, r0		  @; sumamos al valor inicial el resultado y lo guardamos en r3
+				strb r3, [r4, r6]	  @; subimos a la matriz de juego el resultado para cuenta_repeticiones
+				
+				mov r0, r4			   @; volvemos a poner la direccion base de la matriz de juego en r0
+				mov r3, #2			   @; direccion 2
+				bl cuenta_repeticiones @; llamamos a cuenta_repeticiones para comprovar que no tenemos secuencias
+				cmp r0, #3			   @; comparamos el resultado de cuenta_repeticiones con 3
+				bge .L_random 		   @; si >= 3, es que tenemos secuencia de >= 3
+				
+				mov r0, r4			   @; volvemos a poner la direccion base de la matriz de juego en r0
+				mov r3, #3			   @; direccion 3 (REPETIMOS PROCESO EN DIRECCION 3)
+				bl cuenta_repeticiones 
+				cmp r0, #3			   
+				bge .L_random 		   
+				
+			.L_fi:
+				add r6, #1 @; avanza desplazamiento de posiciones						   
+				add r2, #1 
+				cmp r2, #COLUMNS @; comparamos con el numero de columnas para recorerlas todas
+				blo .L_forj
+			add r1, #1
+			cmp r1, #ROWS @;comparamos con el numero de filas para recorerlas todas
+			blo .L_fori
+		pop {r0-r8, pc}			
 
 @;TAREA 1B;
 @; recombina_elementos(*matriz): rutina para generar una nueva matriz de juego
@@ -90,12 +144,35 @@ recombina_elementos:
 @;		R0 = el rango del número aleatorio (n)
 @;	Resultado:
 @;		R0 = el número aleatorio dentro del rango especificado (0..n-1)
+@;=                                                               		=
+@;=== candy1_init.s: rutinas para inicializar la matriz de juego	  ===
+@;=                                                               		=
+
 	.global mod_random
 mod_random:
-		push {lr}
+		push {r2-r4, lr}
 		
+		cmp r0, #2				@;compara el rango de entrada con el mínimo
+		movlo r0, #2			@;si menor, fija el rango mínimo
+		and r0, #0xFF			@;filtra los 8 bits de menos peso
+		sub r2, r0, #1			@;R2 = R0-1 (número más alto permitido)
+		mov r3, #1				@;R3 = máscara de bits
+	.Lmodran_forbits:
+		cmp r3, r2				@;genera una máscara superior al rango requerido
+		bhs .Lmodran_loop
+		mov r3, r3, lsl #1
+		orr r3, #1				@;inyecta otro bit
+		b .Lmodran_forbits
 		
-		pop {pc}
+	.Lmodran_loop:
+		bl random				@;R0 = número aleatorio de 32 bits
+		and r4, r0, r3			@;filtra los bits de menos peso según máscara
+		cmp r4, r2				@;si resultado superior al permitido,
+		bhi .Lmodran_loop		@; repite el proceso
+		mov r0, r4
+		
+		pop {r2-r4, pc}
+
 
 
 
