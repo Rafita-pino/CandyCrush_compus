b74d04cd22dee7d6401734b332291070a0f07e2a
Gerard Ros <gerard.ros@estudiants.urv.cat>
2024-10-11 20:08:59 +0200

    Primera rutina + auxiliars

--- source/candy1_comb.s
+++ source/candy1_comb.s
@@ -74,6 +74,109 @@ hay_combinacion:
 				cmp r7, r8
 				beq .Lif_es_ultima_fila
 			.Lendif_igual_posterior_horitzontal:
+
+			.Lif_posterior_horitzontal_valida:
+				and r11, r8, #0x07
+				cmp r11, #0x07
+				beq .Lif_es_ultima_fila
+				cmp r11, #0x00
+				beq .Lif_es_ultima_fila
+			.Lendif_posterior_horitzontal_valida:
+			
+			@; Intercanvi horitzontal
+				strb r8, [r4, r3]
+				add r3, #1
+				strb r7, [r4, r3]
+				sub r3, #1
+				
+			@; Comprovar primera casella
+				bl detectar_orientacion
+				cmp r0, #6
+				bne .Lif_sequencia_trobada_horitzontal
+				
+			@; Comprovar segona casella
+				add r2, #1
+				bl detectar_orientacion
+				sub r2, #1
+				cmp r0, #6
+				bne .Lif_sequencia_trobada_horitzontal
+				
+			@; Desfer intercanvi horitzontal
+				strb r7, [r4, r3]
+				add r3, #1
+				strb r8, [r4, r3]
+				sub r3, #1
+			
+			.Lif_es_ultima_fila:
+				sub r9, r6, #1 @; r9 = files - 1
+				cmp r1, r9
+				bge .Lendwhile
+			.Lendif_es_ultima_fila:
+			
+			.Lif_posterior_vertical_igual:
+				add r3, r5 @; r3 = r3 + columnes
+				ldrb r10, [r4, r3] @; r10 = casilla posterior vertical
+				sub r3, r5
+				cmp r7, r10
+				beq .Lendwhile
+			.Lendif_posterior_vertical_igual:
+			
+			.Lif_posterior_vertical_valida:
+				and r11, r10, #0x07
+				cmp r11, #0x07
+				beq .Lendwhile
+				cmp r11, #0x00
+				beq .Lendwhile
+			.Lendif_posterior_vertical_valida:
+			
+			@; Intercanvi vertical
+				strb r10, [r4, r3]
+				add r3, r5
+				strb r7, [r4, r3]
+				sub r3, r5 @; r3 = r3 - columnes
+				
+				
+			@; Comprovar primera casella
+				bl detectar_orientacion
+				cmp r0, #6
+				bne .Lif_sequencia_trobada_vertical
+				
+				
+			@; Comprovar segona casella
+				add r1, #1
+				bl detectar_orientacion
+				sub r1, #1
+				cmp r0, #6
+				bne .Lif_sequencia_trobada_vertical
+				
+			@; Desfer intercanvi vertical
+				strb r7, [r4, r3]
+				add r3, r5 @; r3 = r3 + columnes
+				strb r10, [r4, r3]
+				sub r3, r5 @; r3 = r3 - columnes
+			b .Lendwhile
+			
+			.Lif_sequencia_trobada_horitzontal:
+				@; Desfer intercanvi horitzontal
+				strb r7, [r4, r3]
+				add r3, #1
+				strb r8, [r4, r3]
+				sub r3, #1
+				
+				mov r0, #1
+				b .Lfi
+			.Lifend_sequencia_trobada_horitzontal:
+			
+			.Lif_sequencia_trobada_vertical:
+				@; Desfer intercanvi vertical
+				strb r7, [r4, r3]
+				add r3, r5 @; r3 = r3 + columnes
+				strb r10, [r4, r3]
+				sub r3, r5
+				
+				mov r0, #1
+				b .Lfi
+			.Lendif_sequencia_trobada_horitzontal:
 			
 		.Lendwhile:
 		mov r0, #0
@@ -144,10 +247,112 @@ sugiere_combinacion:
 @;	Resultado:
 @;		vector de posiciones (x1,y1,x2,y2,x3,y3), devuelto por referencia
 genera_posiciones:
-		push {lr}
+		push {r1-r5, lr}
 		
+		.Lprimer_punt:
+			cmp r4, #0
+			beq .Lcpi_0
+			cmp r4, #1
+			beq .Lcpi_1
+			cmp r4, #2
+			beq .Lcpi_2
+			cmp r4, #3
+			beq .Lcpi_3
+			
+			.Lcpi_0:
+				add r5, r2, #1		@; p.i a la derecha
+				strb r5, [r0, #0]	@; r5 = x1
+				strb r1, [r0, #1]	@; r1 = y1
+				b .Lendprimer_punt
+			.Lendcpi_0:
+			.Lcpi_1:
+				sub r5, r2, #1		@; p.i a la izquierda
+				strb r5, [r0, #0]	@; r5 = x1
+				strb r1, [r0, #1]	@; r1 = y1
+				b .Lendprimer_punt
+			.Lendcpi_1:
+			.Lcpi_2:
+				add r5, r1, #1		@; p.i hacia abajo
+				strb r5, [r0, #1]	@; r5 = y1
+				strb r2, [r0, #0]	@; r2 = x1
+				b .Lendprimer_punt
+			.Lendcpi_2:
+			.Lcpi_3:
+				sub r5, r1, #1		@; p.i hacia arriba
+				strb r5, [r0, #1]	@; r5 = y1
+				strb r2, [r0, #0]	@; r2 = x1
+			.Lendcpi_3:
+		.Lendprimer_punt:
 		
-		pop {pc}
+		.Laltres_punts:
+			cmp r3, #0
+			beq .Lcori_0
+			cmp r3, #1
+			beq .Lcori_1
+			cmp r3, #2
+			beq .Lcori_2
+			cmp r3, #3
+			beq .Lcori_3
+			cmp r3, #4
+			beq .Lcori_4
+			cmp r3, #5
+			beq .Lcori_5
+			
+			.Lcori_0:
+				add r5, r2, #1	@; p.i al este (1)
+				strb r5, [r0, #2]	@; r5 = x2
+				strb r1, [r0, #3]	@; r1 = y2
+				add r5, #1	@; p.i al este (2)
+				strb r5, [r0, #4]	@; r5 = x2
+				strb r1, [r0, #5]	@; r1 = y2
+				b .Lendaltres_punts
+			.Lendcori_0:
+			.Lcori_1:
+				add r5, r1, #1	@; p.i al sur (1)
+				strb r5, [r0, #3]	@; r5 = y2
+				strb r2, [r0, #2]	@; r2 = x2
+				add r5, #1	@; p.i al sur (2)
+				strb r5, [r0, #5]	@; r5 = y3
+				strb r2, [r0, #4]	@; r2 = x3
+				b .Lendaltres_punts
+			.Lendcori_1:
+			.Lcori_2:
+				sub r5, r2, #1	@; p.i al oeste (1)
+				strb r5, [r0, #2]	@; r5 = x2
+				strb r1, [r0, #3]	@; r1 = y2
+				sub r5, #1	@; p.i al oeste (2)
+				strb r5, [r0, #4]	@; r5 = x2
+				strb r1, [r0, #5]	@; r1 = y2
+				b .Lendaltres_punts
+			.Lendcori_2:
+			.Lcori_3:
+				sub r5, r1, #1	@; p.i al norte (1)
+				strb r5, [r0, #3]	@; r5 = y2
+				strb r2, [r0, #2]	@; r2 = x2
+				sub r5, #1	@; p.i al norte (2)
+				strb r5, [r0, #5]	@; r5 = y3
+				strb r2, [r0, #4]	@; r2 = x3
+				b .Lendaltres_punts
+			.Lendcori_3:
+			.Lcori_4:
+				sub r5, r2, #1	@; p.i al oeste (1)
+				strb r5, [r0, #2]	@; r5 = x2
+				strb r1, [r0, #3]	@; r1 = y2
+				add r5, #2	@; p.i al este (1)
+				strb r5, [r0, #4]	@; r5 = x3
+				strb r1, [r0, #5]	@; r1 = y3 
+				b .Lendaltres_punts
+			.Lendcori_4:
+			.Lcori_5:
+				sub r5, r1, #1	@; p.i al sur (1)
+				strb r5, [r0, #3]	@; r5 = y2
+				strb r2, [r0, #2]	@; r1 = x2
+				add r5, #2	@; p.i al norte (1)
+				strb r5, [r0, #5]	@; r5 = y3
+				strb r2, [r0, #4]	@; r1 = x3
+			.Lendcori_5:
+		.Lendaltres_punts:
+	pop {r1-r5, pc}
 
 
 
@@ -173,10 +378,47 @@ genera_posiciones:
 @;				en medio de secuencia: 4 -> horizontal, 5 -> vertical
 @;				sin secuencia: 6 
 detecta_orientacion:
-		push {lr}
+		push {r3, r5, lr}
 		
+		mov r5, #0				@;R5 = índice bucle de orientaciones
+		mov r0, #4
+		bl mod_random
+		mov r3, r0				@;R3 = orientación aleatoria (0..3)
+	.Ldetori_for:
+		mov r0, r4
+		bl cuenta_repeticiones
+		cmp r0, #1
+		beq .Ldetori_cont		@;no hay inicio de secuencia
+		cmp r0, #3
+		bhs .Ldetori_fin		@;hay inicio de secuencia
+		add r3, #2
+		and r3, #3				@;R3 = salta dos orientaciones (módulo 4)
+		mov r0, r4
+		bl cuenta_repeticiones
+		add r3, #2
+		and r3, #3				@;restituye orientación (módulo 4)
+		cmp r0, #1
+		beq .Ldetori_cont		@;no hay continuación de secuencia
+		tst r3, #1
+		bne .Ldetori_vert
+		mov r3, #4				@;detección secuencia horizontal
+		b .Ldetori_fin
+	.Ldetori_vert:
+		mov r3, #5				@;detección secuencia vertical
+		b .Ldetori_fin
+	.Ldetori_cont:
+		add r3, #1
+		and r3, #3				@;R3 = siguiente orientación (módulo 4)
+		add r5, #1
+		cmp r5, #4
+		blo .Ldetori_for		@;repetir 4 veces
 		
-		pop {pc}
+		mov r3, #6				@;marca de no encontrada
+		
+	.Ldetori_fin:
+		mov r0, r3				@;devuelve orientación o marca de no encontrada
+		
+		pop {r3, r5, pc}
 
 
 
