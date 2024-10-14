9880410b1fc7fa94a9b385cc176e1fc6fa307fb7
Oupman Miralles <oupman.miralles@urv.cat>
2024-10-14 20:28:22 +0200

    mapas de configuracion Tareas 1E 1C

--- source/candy1_conf.s
+++ source/candy1_conf.s
@@ -49,19 +49,63 @@
 		.byte 3, 4, 5, 1, 6, 2, 3, 5, 1
 		.byte 5, 3, 2, 6, 4, 1, 5, 2, 3
 
-	@; mapa 1: con secuencias horizontales y verticales
-		.byte 1, 1, 1, 2, 4, 5, 1, 3, 6
-		.byte 2, 4, 3, 3, 3, 6, 4, 5, 2
-		.byte 6, 5, 6, 1, 1, 1, 3, 2, 4
-		.byte 1, 1, 1, 4, 3, 5, 6, 1, 3
-		.byte 2, 6, 3, 5, 2, 4, 6, 6, 6
-		.byte 3, 2, 1, 3, 3, 3, 2, 5, 4
-		.byte 4, 3, 4, 6, 5, 2, 4, 3, 1
-		.byte 5, 5, 5, 1, 2, 6, 3, 1, 4
-		.byte 6, 4, 2, 1, 6, 5, 6, 6, 6
+	@; mapa 1: con secuencias horizontales
+		.byte 1, 1, 1, 0, 0, 0, 0, 0, 0
+		.byte 0, 0, 0, 2, 2, 2, 0, 0, 0
+		.byte 0, 0, 0, 0, 0, 0, 3, 3, 3
+		.byte 0, 0, 0, 0, 0, 0, 0, 0, 0
+		.byte 0, 0, 0, 0, 0, 0, 0, 0, 0
+		.byte 0, 0, 0, 0, 0, 0, 0, 0, 0
+		.byte 0, 0, 0, 0, 0, 0, 0, 0, 0
+		.byte 0, 0, 0, 0, 0, 0, 0, 0, 0
+		.byte 0, 0, 0, 0, 0, 0, 0, 0, 0
 
+	@; mapa 2: con secuencias verticales
+		.byte 4, 0, 0, 0, 0, 0, 0, 0, 0
+		.byte 4, 0, 0, 0, 0, 0, 0, 0, 0
+		.byte 4, 0, 0, 0, 0, 0, 0, 0, 0
+		.byte 0, 5, 0, 0, 0, 0, 0, 0, 0
+		.byte 0, 5, 0, 0, 0, 0, 0, 0, 0
+		.byte 0, 5, 0, 0, 0, 0, 0, 0, 0
+		.byte 0, 0, 6, 0, 0, 0, 0, 0, 0
+		.byte 0, 0, 6, 0, 0, 0, 0, 0, 0
+		.byte 0, 0, 6, 0, 0, 0, 0, 0, 0
+		
+	@; mapa 3: con secuencias verticales y horizontales sin cruce
+		.byte 7, 7, 7, 0, 0, 0, 0, 0, 0
+		.byte 0, 0, 0, 8, 8, 8, 0, 0, 0
+		.byte 0, 0, 0, 0, 0, 0, 9, 9, 9
+		.byte 10, 0, 0, 0, 0, 0, 0, 0, 0
+		.byte 10, 0, 0, 0, 0, 0, 0, 0, 0
+		.byte 10, 0, 0, 0, 0, 0, 0, 0, 0
+		.byte 0, 0, 11, 0, 0, 0, 0, 0, 0
+		.byte 0, 0, 11, 0, 0, 0, 0, 0, 0
+		.byte 0, 0, 11, 0, 0, 0, 0, 0, 0
+		
+	@; mapa 4: con secuencias verticales con cruce de diferentes numeros
+	
+		.byte 12, 12, 12, 0, 0, 0, 0, 0, 0
+		.byte 0, 0, 0, 13, 13, 13, 0, 0, 0
+		.byte 14, 0, 0, 0, 0, 0, 0, 0, 0
+		.byte 14, 0, 0, 15, 15, 15, 0, 0, 0
+		.byte 14, 0, 0, 0, 0, 0, 0, 0, 0
+		.byte 0, 16, 0, 0, 0, 0, 0, 0, 0
+		.byte 0, 16, 0, 0, 0, 0, 0, 0, 0
+		.byte 0, 16, 17, 17, 17, 0, 0, 0, 0
+		.byte 0, 0, 0, 0, 0, 0, 0, 0, 0
 
-	@; mapa 2: con bloques sólidos y huecos
+	@; mapa 5: con secuencias verticales con cruce de mismos numeros
+		.byte 18, 18, 18, 0, 0, 0, 0, 0, 0
+		.byte 0, 0, 0, 18, 18, 18, 0, 0, 0
+		.byte 18, 0, 0, 18, 0, 0, 0, 0, 0
+		.byte 18, 0, 0, 18, 18, 18, 0, 0, 0
+		.byte 18, 0, 0, 0, 0, 0, 0, 0, 0
+		.byte 0, 18, 0, 0, 0, 0, 0, 0, 0
+		.byte 0, 18, 0, 0, 0, 0, 0, 0, 0
+		.byte 0, 18, 18, 18, 0, 0, 0, 0, 0
+		.byte 0, 0, 0, 0, 0, 0, 0, 0, 0
+
+	@; mapa 6: con bloques sólidos y huecos
 		.byte 1, 7, 1, 2, 4, 7, 1, 15, 6
 		.byte 2, 15, 3, 3, 15, 6, 4, 5, 7
 		.byte 6, 7, 7, 1, 15, 1, 3, 2, 4
@@ -73,7 +117,7 @@
 		.byte 6, 7, 2, 1, 6, 5, 15, 7, 6
 
 	
-	@; mapa 3: con gelatinas simples y dobles
+	@; mapa 7: con gelatinas simples y dobles
 		.byte 9, 10, 11, 2, 17, 18, 1, 3, 6
 		.byte 2, 4, 3, 14, 14, 6, 4, 5, 19
 		.byte 6, 5, 6, 11, 1, 1, 3, 2, 4
@@ -85,7 +129,7 @@
 		.byte 6, 4, 2, 1, 22, 5, 6, 6, 6
 
 
-	@; mapa 4: secuencias en los límites
+	@; mapa 8: secuencias en los límites
 		.byte 1, 2, 3, 4, 5, 6, 1, 1, 1
 		.byte 4, 5, 6, 1, 2, 3, 6, 6, 6
 		.byte 3, 2, 1, 4, 5, 6, 4, 4, 4
@@ -96,7 +140,7 @@
 		.byte 6, 3, 4, 1, 5, 1, 6, 3, 1
 		.byte 3, 2, 1, 6, 6, 6, 5, 4, 3
 
-	@; mapa 5: secuencias en horizontal de 3, 4 y 5 elementos
+	@; mapa 9: secuencias en horizontal de 3, 4 y 5 elementos
 		.byte 1,1,1,15,2,2,2,2,7
 		.byte 3,3,3,3,3,15,7,7,15
 		.byte 4,1,4,4,4,4,15,7,15
@@ -107,7 +151,7 @@
 		.byte 7,7,7,15,7,7,7,15,15
 		.byte 15,15,7,15,15,15,7,15,15
 
-	@; mapa 6: secuencias en vertical de 3, 4 y 5 elementos
+	@; mapa 10: secuencias en vertical de 3, 4 y 5 elementos
 		.byte 1,3,4,1,5,6,2,15,15
 		.byte 1,3,1,4,2,5,7,15,15
 		.byte 1,3,4,4,2,5,15,7,15
@@ -118,7 +162,7 @@
 		.byte 7,15,15,7,7,5,6,7,15
 		.byte 15,15,7,15,15,5,7,15,15
 
-	@; mapa 7: combinaciones cruzadas (hor/ver) de 5, 6 y 7 elementos
+	@; mapa 11: combinaciones cruzadas (hor/ver) de 5, 6 y 7 elementos
 		.byte 15,15,7,15,15,7,15,15,15
 		.byte 1,2,3,3,4,3,7,0,15
 		.byte 1,2,7,5,3,7,7,0,15
@@ -129,7 +173,7 @@
 		.byte 7,8,1,5,4,6,8,0,15
 		.byte 8,8,8,8,8,8,8,0,15
 		
-	@; mapa 8: no hay combinaciones ni secuencias
+	@; mapa 12: no hay combinaciones ni secuencias
 		.byte 15,15,15,15,15,15,15,15,15
 		.byte 15,15,15,15,15,15,15,15,15
 		.byte 1,2,3,3,7,3,15,15,15
